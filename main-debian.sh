d01eb98f2d0ace07bcff0225f5c46d89e6658706
Patryk Golabek <pgolabek@gmail.com>
2016-06-06 15:44:28 -0400

    add backports for docker install

--- main-debian.sh
+++ main-debian.sh
@@ -7,6 +7,7 @@ sudo apt-get purge docker.io*
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 #empty file
 echo -n /etc/apt/sources.list.d/docker.list
+echo "deb http://http.debian.net/debian wheezy-backports main" >> /etc/apt/sources.list.d/docker.list
 sudo apt-cache policy docker-engine
 sudo apt-get update
 echo "=> Done!"
